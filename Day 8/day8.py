8c90aede8c2ffe850a04d59ce5a1fbe376866c66
Joshtblanchard <Joshtblanchard@gmail.com>
2022-12-13 12:23:32 -0500

    day 8 changes

--- Day 8/day8.py
+++ Day 8/day8.py
@@ -1,15 +1,7 @@
 data = open('Day 8/input.txt', 'r',).read().splitlines()
 
-# print (data)x
-
 # I need to enter the line ahead and the line behind into memory,
 # Then reference them in the checks to see if a tree is visable.
-above = []
-
-
-below = []
-right = []
-left = []
 
 def fistline():
     for line in data:
@@ -22,3 +14,15 @@ def is_visible(tree):
         line.strip
         prev_line = line
         for x, tree in enumerate(line):
+            print("I don't like where this is going.....")
+
+
+grid = [list(map(int, line)) for line in data]
+t = 0
+
+for r in range(len(grid)):
+    for c in range(len(grid[r])):
+        k = grid[r][c]
+        if all(grid[r][x] < k for x in range(c)) or all(grid[r][x] < k for x in range(c+1, len(grid[r]))) or all(grid[x][c]<k for x in range(r)) or all (grid[x][c]<k for x in range(r +1, len(grid))):
+            t +=1
+print (t)

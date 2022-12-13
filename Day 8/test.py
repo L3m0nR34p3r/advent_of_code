7aa0197b427ca1e57fd02f51e44f4397436a1d20
Joshtblanchard <Joshtblanchard@gmail.com>
2022-12-13 12:23:21 -0500

    Trying to solve question...

---
+++ Day 8/test.py
@@ -0,0 +1,28 @@
+def maxCost(cost, labels, dailyCount):
+    passed=[]
+    failed=[]
+    max_daily_cost=[]
+    laptop = zip(cost,labels)
+    laptop=list(laptop)
+    chunked_list = list()
+    for i in range(0,len(laptop),dailyCount):
+        if laptop(y[1] for y in laptop) == 'illegal':
+            failed.append(y[1] for y in laptop)
+        elif laptop(y[1] for y in laptop) == 'legal':
+            passed.append(y[1] for y in laptop)
+        chunked_list.append(laptop[i:i+dailyCount])
+    print(passed)
+    # for x in chunked_list:
+    #     # print(x)
+    #     for a in x:
+    #         # print(a)
+    #         if a[1] == 'legal':
+    #             max_daily_cost.append(a[0])
+    # # print(chunked_list)
+
+
+cost=[0,3,2,3,4]
+label = ['legal','legal','illegal','legal','legal',]
+count = 2 # 'legal' per day
+total = maxCost(cost,label,count)
+print(total)
